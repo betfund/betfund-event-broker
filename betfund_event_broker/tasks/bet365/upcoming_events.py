6e6c2f8ef479ef9d2f3f35a0ac24803d573b864b
Leon Kozlowski <LeonKozlowski@gmail.com>
2020-04-24 12:48:08 -0400

    Bugfix/logging (#7)
    * fix logging issue
    
    * bump version
    
    * fix flake error

--- betfund_event_broker/tasks/bet365/upcoming_events.py
+++ betfund_event_broker/tasks/bet365/upcoming_events.py
@@ -45,8 +45,11 @@ class Bet365UpcomingEvents(Bet365Task):
         sport_name = sport[1]
 
         response = bet365_client.upcoming_events(sport_id=sport_id)
-        logger.info(
-            f"<{sport_name.upper()}> RETURNED {len(response.results)} RECORDS"
-        )
+
+        if response.results:
+            logger.info(
+                f"<{sport_name.upper()}>"
+                f" RETURNED {len(response.results)} RECORDS"
+            )
 
         return response
